558e2f37958d35c7da77375355102dec2b29d865
rjrobins16 <rjrobins16@gmail.com>
2019-08-29 10:44:54 -0500

    final w/o challenge

--- index.py
+++ index.py
@@ -1,28 +1,37 @@
 arrayOfTasks = []
 userInput = ""
+print("Congratulations! You're running R&J's Task List program.")
+
 
 def addTask():
     newTask = input("Enter a new task to add to your list.")
     arrayOfTasks.append(newTask)
-def listTask(array):
-    for x in array:
-        print(x)
 
-while userInput != 0 :
-    userInput = int(input(f'What would you like to do next?'
-                      f'1. List all tasks.'
-                      f'2. Add a task to the list.'
-                      f'3. Delete a task.'
-                      f'0. To quit the program'))
+
+def listTask(array):
+    if len(array) != 0:
+        for x in array:
+            print(x)
+    else:
+        print("Your task list is empty.")
+
+
+def deleteTask(array):
+    userIn = input("Which task to delete?")
+    array.remove(userIn)
+
+
+while userInput != 0:
+    userInput = int(input(f'What would you like to do next?\n'
+                          f'1. List all tasks.\n'
+                          f'2. Add a task to the list.\n'
+                          f'3. Delete a task.\n'
+                          f'0. To quit the program'))
     if userInput == 1:
         listTask(arrayOfTasks)
 
     if userInput == 2:
         addTask()
 
-
-
-print (arrayOfTasks[0])
-
-
-
+    if userInput == 3:
+        deleteTask(arrayOfTasks)
